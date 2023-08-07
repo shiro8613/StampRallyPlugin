c85be0b64ed55cd69f635334df6cdd706f856dd6
toms0910 <psp1070@gmail.com>
2023-08-07 13:39:29 +0900

    Initial commit

---
+++ src/main/java/dev/shiro8613/stamprallyplugin/StampRallyPlugin.java
@@ -0,0 +1,17 @@
+package dev.shiro8613.stamprallyplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StampRallyPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
