54a620d92594dfcf4600fbe45f1fdbbddf18ee15
toms0910 <psp1070@gmail.com>
2023-08-08 04:21:07 +0900

    fix Distance Calc

--- src/main/java/dev/shiro8613/stamprallyplugin/utils/DistanceLocation.java
+++ src/main/java/dev/shiro8613/stamprallyplugin/utils/DistanceLocation.java
@@ -1,21 +1,23 @@
 package dev.shiro8613.stamprallyplugin.utils;
 
+import dev.shiro8613.stamprallyplugin.database.entry.StampLocation;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
 public class DistanceLocation {
-    public static void CalcRun(int radius, Location centerLocation, Location location, Context ctx) {
-        int PosX1 = centerLocation.getBlockX();
-        int PosY1 = centerLocation.getBlockY();
-        int PosZ1 = centerLocation.getBlockZ();
+    public static void CalcRun(int radius, StampLocation centerLocation, Location location, Context ctx) {
+        int PosX1 = centerLocation.PosX;
+        int PosY1 = centerLocation.PosY;
+        int PosZ1 = centerLocation.PosZ;
 
-        int PosX2 = location.getBlockX();
-        int PosY2 = location.getBlockY();
-        int PosZ2 = location.getBlockZ();
+        int PosX2 = (int) Math.round(location.getX());
+        int PosY2 = (int) Math.round(location.getY());
+        int PosZ2 = (int) Math.round(location.getZ());
 
         for (int x = -radius; x <= radius; x++) {
             for (int y = -radius; y <= radius; y++) {
                 for (int z = -radius; z <= radius; z++) {
-                    if (PosX2 == PosX1 + x && PosY2 == PosY1 + y && PosZ2 == PosZ1 + z) {
+                    if ((PosX1 + x) == PosX2 && (PosY1 + y) == PosY2 && (PosZ1 + z) == PosZ2) {
                         ctx.Run();
                     }
                 }
