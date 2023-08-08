c98d8a03cf9c038c817a2593629b9efa8e494dd0
toms0910 <psp1070@gmail.com>
2023-08-08 12:16:23 +0900

    add item text

--- src/main/java/dev/shiro8613/stamprallyplugin/map/MapManager.java
+++ src/main/java/dev/shiro8613/stamprallyplugin/map/MapManager.java
@@ -3,12 +3,17 @@ package dev.shiro8613.stamprallyplugin.map;
 import dev.shiro8613.stamprallyplugin.database.Database;
 import dev.shiro8613.stamprallyplugin.memory.DataStore;
 import dev.shiro8613.stamprallyplugin.utils.json.StampData;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.MapMeta;
 import org.bukkit.map.MapView;
+
+import java.util.List;
 
 public class MapManager {
 
@@ -19,6 +24,13 @@ public class MapManager {
         DataStore.LoadMapStampData();
 
         ItemStack itemStack = new ItemStack(Material.FILLED_MAP);
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.displayName(Component.text("スタンプラリーカード", NamedTextColor.AQUA));
+        meta.lore(List.of(Component.text("各地をまわってスタンプを集め、「", NamedTextColor.GRAY)
+                .append(Component.text("景品", NamedTextColor.GOLD))
+                .append(Component.text("」を手に入れよう", NamedTextColor.GRAY))));
+
+        itemStack.setItemMeta(meta);
         MapMeta mapMeta = (MapMeta) itemStack.getItemMeta();
 
         CustomMapRenderer.ReloadRenderer(mapView.getId());
