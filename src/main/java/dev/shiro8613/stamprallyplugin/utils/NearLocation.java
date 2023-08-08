514860e0d44531585c52d12a1982d54f62b4bc20
toms0910 <psp1070@gmail.com>
2023-08-08 16:25:29 +0900

    fix nearlocation calc logic

--- src/main/java/dev/shiro8613/stamprallyplugin/utils/NearLocation.java
+++ src/main/java/dev/shiro8613/stamprallyplugin/utils/NearLocation.java
@@ -3,9 +3,7 @@ package dev.shiro8613.stamprallyplugin.utils;
 import dev.shiro8613.stamprallyplugin.database.entry.StampLocation;
 import org.bukkit.Location;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class NearLocation {
     public static StampLocation Calc(List<StampLocation> l1, Location l2) {
@@ -13,18 +11,28 @@ public class NearLocation {
                 .filter(l -> l.WorldName.equals(l2.getWorld().getName()))
                 .toList();
 
-        Map<Double, StampLocation> map = new HashMap<>();
-        list.forEach(l -> {
-            double d = Math.sqrt(
-                            ((Math.abs(l2.getBlockX()) - Math.abs(l.PosX)) * 2) +
-                            ((Math.abs(l2.getBlockY()) - Math.abs(l.PosY)) * 2) +
-                            ((Math.abs(l2.getBlockZ()) - Math.abs(l.PosZ)) * 2)
+        double bef = 0;
+        StampLocation ret = null;
+
+        for (int i = 0; i < list.size(); i++) {
+            StampLocation l = list.get(i);
+            double d = (
+                    Math.pow((l2.getBlockX() - l.PosX), 2) +
+                            Math.pow((l2.getBlockY() - l.PosY), 2) +
+                            Math.pow((l2.getBlockZ() - l.PosZ), 2)
             );
-            map.put(d, l);
-        });
 
-        double min = map.keySet().stream().sorted().toList().get(0);
+            if (i == 0) {
+                bef = d;
+                ret = l;
+            } else {
+                if (bef >= d) {
+                    bef = d;
+                    ret = l;
+                }
+            }
+        }
 
-        return map.get(min);
+        return ret;
     }
 }
