5413481dfaf553a80be67482f7beaea0abe5fa27
toms0910 <psp1070@gmail.com>
2023-08-12 14:50:35 +0900

    fix rendering timings and add flying player not call func and change async

--- src/main/java/dev/shiro8613/stamprallyplugin/Event.java
+++ src/main/java/dev/shiro8613/stamprallyplugin/Event.java
@@ -15,6 +15,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Map;
 
@@ -24,31 +25,35 @@ public class Event implements Listener {
     public void onSneak(PlayerToggleSneakEvent event) {
         Player player = event.getPlayer();
         if (event.isSneaking()) return;
+        if (player.isFlying()) return;
         PlayerInventory inventory = player.getInventory();
 
         int mapId = HandItem.getMapId(inventory);
         if(!DataStore.getMapStamp().containsKey(mapId)) return;
 
-        StampLocation nearStampLocation = NearLocation.Calc(DataStore.getLocations(), player.getLocation());
-        if(nearStampLocation == null) return;
+        JavaPlugin plugin = StampRallyPlugin.getInstance();
+        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+            StampLocation nearStampLocation = NearLocation.Calc(DataStore.getLocations(), player.getLocation());
+            if (nearStampLocation == null) return;
 
-        int radius = DataStore.getConfig().radius;
-        DistanceLocation.CalcRun(radius, nearStampLocation, player.getLocation(), (radius + 7), () -> {
-            String data = DataStore.getMapStamp().get(mapId);
-            if (data == null) return;
+            int radius = DataStore.getConfig().radius;
+            DistanceLocation.CalcRun(radius, nearStampLocation, player.getLocation(), (radius + 7), () -> {
+                String data = DataStore.getMapStamp().get(mapId);
+                if (data == null) return;
 
-            Map<Integer, Boolean> map = StampData.DecodeStamps(data);
-            if (map != null && map.containsKey(nearStampLocation.StampId) && !map.get(nearStampLocation.StampId)) {
-                map.put(nearStampLocation.StampId, true);
-            }
+                Map<Integer, Boolean> map = StampData.DecodeStamps(data);
+                if (map != null && map.containsKey(nearStampLocation.StampId) && !map.get(nearStampLocation.StampId)) {
+                    map.put(nearStampLocation.StampId, true);
 
-            if (Database.getInstance().setMapStamp(mapId, StampData.EncodeStamps(map))) {
-                DataStore.LoadMapStampData();
-                CustomMapRenderer.ReloadRenderer(mapId);
-            }
-        }, () -> player.sendMessage(Component.text("取得可能範囲中心より", NamedTextColor.WHITE)
-                    .append(Component.text("「" + (radius + 7) +"ブロック」", NamedTextColor.YELLOW))
+                    if (Database.getInstance().setMapStamp(mapId, StampData.EncodeStamps(map))) {
+                        DataStore.LoadMapStampData();
+                        CustomMapRenderer.ReloadRenderer(mapId);
+                    }
+                }
+            }, () -> player.sendMessage(Component.text("取得可能範囲中心より", NamedTextColor.WHITE)
+                    .append(Component.text("「" + (radius + 7) + "ブロック」", NamedTextColor.YELLOW))
                     .append(Component.text("の範囲に入っています。もう少し近づいて再度お試しください。", NamedTextColor.WHITE))));
-    }
 
+        });
+    }
 }
